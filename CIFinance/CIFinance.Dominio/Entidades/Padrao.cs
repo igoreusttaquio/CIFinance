f5e2d77ce303ec75609c2eab5471eafc634982ea
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-03 22:30:49 -0300

    Classe padrao para todas as outras entidades.

---
+++ CIFinance/CIFinance.Dominio/Entidades/Padrao.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIFinance.Dominio.Entidades;
+
+public abstract class Padrao
+{
+    public int Id { get; protected set; }
+    public DateTime Criacao { get; protected set; } = DateTime.UtcNow;
+    public DateTime UltimaAlteracao { get; protected set; } = DateTime.UtcNow;
+    [MaxLength(10)]
+    public string IdentificadorExterno { get; protected set; } = Identificador.Novo();
+    public bool Ativo { get; protected set; } = true;
+
+    public abstract void Atualizar<TEntidade>(TEntidade entidade);
+
+    public void Inativar()
+    {
+        Ativo = false;
+        AtualizarDataUltimaAlteracao();
+    }
+
+    public void AtualizarDataUltimaAlteracao()
+    {
+        UltimaAlteracao = DateTime.UtcNow;
+    }
+}
