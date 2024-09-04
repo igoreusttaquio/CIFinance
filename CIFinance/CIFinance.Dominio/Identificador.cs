7bacbbbdfeef8c76af8d2bf255831ff4f7729e31
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-03 22:31:05 -0300

    Classe para obter identificador externo.

---
+++ CIFinance/CIFinance.Dominio/Identificador.cs
@@ -0,0 +1,26 @@
+﻿using Sqids;
+
+namespace CIFinance.Dominio;
+
+public static class Identificador
+{
+    private static readonly SqidsOptions opcoes = new()
+    {
+        Alphabet = "k3G7QAe51FCsPW92uEOyq4Bg6Sp8YzVTmnU0liwDdHXLajZrfxNhobJIRcMvKt",
+    };
+
+    private static readonly SqidsEncoder<int> sqids = new(opcoes);
+
+    private static int NumeroAleatorio => new Random().Next(byte.MinValue, byte.MaxValue);
+
+    public static string Novo()
+    {
+        int limite = 10;
+        var identificador = sqids.Encode(NumeroAleatorio, NumeroAleatorio, NumeroAleatorio);
+        if(identificador.Length > limite)
+        {
+            return identificador[..limite];
+        }
+        return identificador;
+    }
+}
