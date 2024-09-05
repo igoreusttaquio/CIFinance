ce19ee1fbe8e5b8f75c63bdc126ec749a02eac32
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-04 22:53:51 -0300

    Nova entidade para representar uma categoria e uma Transacao.

---
+++ CIFinance/CIFinance.Dominio/Entidades/Categoria.cs
@@ -0,0 +1,39 @@
+﻿using CIFinance.Dominio.Enumeradores;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CIFinance.Dominio.Entidades;
+
+[Table("Categorias")]
+public class Categoria : Entidade
+{
+    [Required]
+    [MaxLength(50)]
+    public string Nome { get; private set; } = null!;
+
+    [Required]
+    [MaxLength(50)]
+    [AllowedValues(nameof(Enumeradores.Tipo.Receita), nameof(Enumeradores.Tipo.Receita))]
+    public string Tipo { get; private set; } = null!;
+    public Usuario Usuario { get; private set; } = null!;
+
+    protected Categoria() { } // Requerido por causa do entity framework
+    public Categoria(string nome, Tipo tipo, Usuario usuario)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(nome, nameof(nome));
+        Nome = nome;
+        Tipo = tipo.ToString();
+        Usuario = usuario;
+    }
+
+    public override void Atualizar<TEntidade>(TEntidade entidade)
+    {
+        if (entidade is Categoria categoria)
+        {
+            Nome = categoria.Nome;
+            Tipo = categoria.Tipo;
+        }
+        else
+            throw new ArgumentException("Entidade especificada nao e do tipo Categoria", nameof(entidade));
+    }
+}
---
+++ CIFinance/CIFinance.Dominio/Entidades/Transacao.cs
@@ -0,0 +1,59 @@
+﻿using CIFinance.Dominio.Enumeradores;
+using CIFinance.Dominio.Extensoes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CIFinance.Dominio.Entidades;
+[Table("Transacoes")]
+public class Transacao : Entidade
+{
+    public Conta Conta { get; private set; } = null!;
+    public Categoria Categoria { get; private set; } = null!;
+
+    [Required]
+    [MaxLength(100)]
+    public string Descricao { get; private set; } = null!;
+
+    [Required]
+    public decimal Valor { get; private set; } = 0m;
+    public DateTime Data { get; private set; } = DateTime.UtcNow;
+
+    [Required]
+    [MaxLength(50)]
+    [AllowedValues(nameof(Enumeradores.Tipo.Receita), nameof(Enumeradores.Tipo.Receita))]
+    public string Tipo { get; private set; } = null!;
+
+    protected Transacao() { } // Requerido por causa do entity framework
+
+    public Transacao(Conta conta, Categoria categoria, string descricao, decimal valor, Tipo tipo, DateTime? data = default)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(descricao, nameof(descricao));
+        if (categoria.Tipo.ParaEnum<Tipo>() != tipo)
+        {
+            var msg = $"Nao e possivel utilizar um tipo de transaceo inconsistente ao tipo da categoria. Informado: {tipo}, Categoria {categoria.Tipo}.";
+            throw new ArgumentException(msg, nameof(tipo));
+        }
+
+        Valor = valor;
+        Conta = conta;
+        Tipo = tipo.ToString();
+        Categoria = categoria;
+        Descricao = descricao;
+        Data = data is null ? DateTime.UtcNow : (DateTime)data;
+    }
+
+    public override void Atualizar<TEntidade>(TEntidade entidade)
+    {
+        if (entidade is Transacao transacao)
+        {
+            Valor = transacao.Valor;
+            Conta = transacao.Conta;
+            Tipo = transacao.Tipo;
+            Categoria = transacao.Categoria;
+            Descricao = transacao.Descricao;
+            Data = transacao.Data;
+        }
+        else
+            throw new NotImplementedException();
+    }
+}
