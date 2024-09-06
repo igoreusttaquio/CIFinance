afaf3d0e8814fe64c76cbfb87a8286c871575c36
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-06 19:18:07 -0300

    Abstracoes para as entidades.

---
+++ CIFinance/CIFinance.Dominio/Abstracoes/IEntidade.cs
@@ -0,0 +1,14 @@
+﻿namespace CIFinance.Dominio.Abstracoes;
+
+public interface IEntidade
+{
+    int Id { get; }
+    DateTime Criacao { get; }
+    DateTime UltimaAlteracao { get; }
+    string IdentificadorExterno { get; }
+    bool Ativo { get; }
+
+    void Atualizar<TEntidade>(TEntidade entidade);
+    void Inativar();
+    void AtualizarDataUltimaAlteracao();
+}
---
+++ CIFinance/CIFinance.Dominio/Abstracoes/IRepositorioEntidade.cs
@@ -0,0 +1,9 @@
+﻿namespace CIFinance.Dominio.Abstracoes;
+
+public interface IRepositorioEntidade
+{
+    void Criar(IEntidade entidade);
+    void Atualizar(IEntidade entidade);
+    void Excluir(IEntidade entidade);
+    IEntidade? Obter(IEntidade entidade);
+}
