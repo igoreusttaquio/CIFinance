ee8822ca8fb5c929673a9891d079fe9fe66e9ba9
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-05 23:52:44 -0300

    Entidade para representar a tabela de orçamentos.

---
+++ CIFinance/CIFinance.Dominio/Entidades/Orcamento.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CIFinance.Dominio.Entidades;
+
+[Table("Orcamentos")]
+public class Orcamento : Entidade
+{
+    public Usuario Usuario { get; private set; } = null!;
+    public Categoria Categoria { get; private set; } = null!;
+    public decimal Valor { get; private set; }
+    public DateTime DataInicio { get; private set; } = DateTime.UtcNow;
+    public DateTime DataFim { get; private set; } = DateTime.UtcNow;
+    public string? Descricao { get; private set; }
+
+    public Orcamento() { }
+    public Orcamento(Usuario usuario, Categoria categoria, decimal valor, DateTime dataInicio, DateTime dataFim, string? descricao = null)
+    {
+        if (dataInicio > DateTime.Today)
+        {
+            throw new ArgumentException("Data informada inválida.");
+        }
+
+        if (dataFim > dataInicio)
+        {
+            throw new ArgumentException("A data de início não pode ser menor que a data de fim.");
+        }
+
+        Usuario = usuario;
+        Categoria = categoria;
+        Valor = valor;
+        DataInicio = dataInicio;
+        DataFim = dataFim;
+        Descricao = descricao;
+    }
+    public override void Atualizar<TEntidade>(TEntidade entidade)
+    {
+        if (entidade is Orcamento orcamento)
+        {
+            Categoria = orcamento.Categoria;
+            Valor = orcamento.Valor;
+            DataInicio = orcamento.DataInicio;
+            DataFim = orcamento.DataFim;
+            Descricao = orcamento.Descricao;
+        }
+        else
+            throw new ArgumentException("Entidade especificada nao e do tipo Orcamento", nameof(entidade));
+    }
+}
