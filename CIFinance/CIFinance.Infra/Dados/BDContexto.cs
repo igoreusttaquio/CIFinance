2e8319be25403e09dd31792103a85d0e2e68370f
Carlos Papa <carloseduardo.f.p@hotmail.com>
2024-09-09 20:51:52 -0300

    Inclusão da Conta no BDContexto e filtro para buscar sempre os ativos.

--- CIFinance/CIFinance.Infra/Dados/BDContexto.cs
+++ CIFinance/CIFinance.Infra/Dados/BDContexto.cs
@@ -7,10 +7,11 @@ public class BDContexto : DbContext
 {
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Usuario> Usuarios { get; set; }
-
+    public DbSet<Conta> Contas { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categoria>().HasQueryFilter(c => c.Ativo);
         modelBuilder.Entity<Usuario>().HasQueryFilter(u => u.Ativo);
+        modelBuilder.Entity<Conta>().HasQueryFilter(c => c.Ativo);
     }
 }
