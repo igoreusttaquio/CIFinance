dcfb73f0757b5538b1aaffe641a0c9f92267a633
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-14 12:16:24 -0300

    Teste token.

--- CIFinance/CIFinance.Testes/Repositorios/TesteUnitarioUsuarioRepositorio.cs
+++ CIFinance/CIFinance.Testes/Repositorios/TesteUnitarioUsuarioRepositorio.cs
@@ -4,141 +4,140 @@ using Microsoft.EntityFrameworkCore;
 using CIFinance.Infra.Repositorio;
 using CIFinance.Dominio.Entidades;
 
-namespace CIFinance.Testes.Repositorios
+namespace CIFinance.Testes.Repositorios;
+
+public class TesteUnitarioUsuarioRepositorio
 {
-    public class TesteUnitarioUsuarioRepositorio
+    private readonly UsuarioRepositorio _usuarioRepositorio;
+    private readonly DbContextOptions<BDContexto> _options;
+    private readonly string _idUsuario;
+    private readonly BDContexto _contexto;
+
+    private readonly string _nomeUsuario = "Igor Silva";
+    private readonly string _emailUsuario = "igoreusttaquio@gmail.com";
+    private readonly string _outroNome = "Silva Igor";
+    private readonly string _outroEmail = "silvaigor@gmail.com";
+
+    public TesteUnitarioUsuarioRepositorio()
     {
-        private readonly UsuarioRepositorio _usuarioRepositorio;
-        private readonly DbContextOptions<BDContexto> _options;
-        private readonly string _idUsuario;
-        private readonly BDContexto _contexto;
+        // arrange
+        _options = new DbContextOptionsBuilder<BDContexto>()
+        .UseInMemoryDatabase(databaseName: "TestDatabase")
+        .Options;
 
-        private readonly string _nomeUsuario = "Igor Silva";
-        private readonly string _emailUsuario = "igoreusttaquio@gmail.com";
-        private readonly string _outroNome = "Silva Igor";
-        private readonly string _outroEmail = "silvaigor@gmail.com";
+        _contexto = new BDContexto(_options);
+        var usuario = Usuario.Fabrica.Criar(_nomeUsuario, _emailUsuario);
+        _contexto.Usuarios.Add(usuario);
+        _contexto.SaveChanges();
 
-        public TesteUnitarioUsuarioRepositorio()
-        {
-            // arrange
-            _options = new DbContextOptionsBuilder<BDContexto>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .Options;
+        _idUsuario = usuario.IdentificadorExterno;
 
-            _contexto = new BDContexto(_options);
-            var usuario = Usuario.Fabrica.Criar(_nomeUsuario, _emailUsuario);
-            _contexto.Usuarios.Add(usuario);
-            _contexto.SaveChanges();
-
-            _idUsuario = usuario.IdentificadorExterno;
-
-            _usuarioRepositorio = new UsuarioRepositorio(_contexto);
-        }
-
-        [Fact]
-        public async void ObterUsuarioPorEmail_RetornaUsuario()
-        {
-            // act
-            var usuario = await _usuarioRepositorio.ObterPorEmailAsync(_emailUsuario);
-
-            // assert
-            Assert.NotNull(usuario);
-        }
-
-        [Fact]
-        public async void ObterUsuarioPorEmail_RetornaUsuarioComNomeCorreto()
-        {
-            // act
-            var usuario = await _usuarioRepositorio.ObterPorEmailAsync(_emailUsuario);
-
-            // assert
-            Assert.NotNull(usuario);
-            Assert.Equal(_nomeUsuario, usuario.Nome);
-        }
-
-        [Fact]
-        public async void ObterUsuarioPorId_RetornaUsuario()
-        {
-            // act 
-            var usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
-
-            // assert
-            Assert.NotNull(usuario);
-        }
-
-        [Fact]
-        public async void ObterUsuarioPorId_RetornaUsuarioComNomeCorreto()
-        {
-            // act 
-            var usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
-
-            // assert
-            Assert.NotNull(usuario);
-            Assert.Equal(_nomeUsuario, usuario.Nome);
-        }
-
-        [Fact]
-        public async void SalvarUsuario_RetornaUsuarioComNomeAlterado()
-        {
-            // act 
-            Usuario? usuario;
-            if (await _usuarioRepositorio.ObterPorIdAsync(_idUsuario) is Usuario u)
-            {
-                u.Atualizar(Usuario.Fabrica.Criar(_outroNome, _emailUsuario));
-                _usuarioRepositorio.Atualizar(u);
-                _contexto.SaveChanges();
-            }
-
-            usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
-
-            // assert
-            Assert.NotNull(usuario);
-            Assert.Equal(_outroNome, usuario.Nome);
-        }
-
-        [Fact]
-        public async void CriarUsuario_RetornaUsuario()
-        {
-            // act 
-            var usuario = Usuario.Fabrica.Criar(_outroNome, _outroEmail);
-
-            await _usuarioRepositorio.CriarAsync(usuario);
-            await _contexto.SaveChangesAsync();
-
-            // assert
-            Assert.NotNull(await _usuarioRepositorio.ObterPorIdAsync(usuario.IdentificadorExterno));
-            Assert.Equal(_outroNome, usuario.Nome);
-        }
-
-        [Fact]
-        public async void ExcluirUsuario_RetornaVerdadeiro()
-        {
-            // act 
-            var usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
-            bool registoExcluido = false;
-
-            if(usuario is not null)
-            {
-                _usuarioRepositorio.Excluir(usuario);
-                registoExcluido = await _contexto.SaveChangesAsync() > 0;
-            }
-
-            // assert
-            Assert.Null(await _usuarioRepositorio.ObterPorIdAsync(_idUsuario));
-            Assert.True(registoExcluido);
-        }
-
-        [Fact]
-        public async void ObterTodosUsuario_RetornaListaUsuarios()
-        {
-            // act 
-            var usuarios = await _usuarioRepositorio.ObterTodosAsync();
-
-
-            // assert
-            Assert.NotNull(usuarios);
-            Assert.True(usuarios is not null and IEnumerable<Usuario>);
-        }
-
+        _usuarioRepositorio = new UsuarioRepositorio(_contexto);
     }
+
+    [Fact]
+    public async void ObterUsuarioPorEmail_RetornaUsuario()
+    {
+        // act
+        var usuario = await _usuarioRepositorio.ObterPorEmailAsync(_emailUsuario);
+
+        // assert
+        Assert.NotNull(usuario);
+    }
+
+    [Fact]
+    public async void ObterUsuarioPorEmail_RetornaUsuarioComNomeCorreto()
+    {
+        // act
+        var usuario = await _usuarioRepositorio.ObterPorEmailAsync(_emailUsuario);
+
+        // assert
+        Assert.NotNull(usuario);
+        Assert.Equal(_nomeUsuario, usuario.Nome);
+    }
+
+    [Fact]
+    public async void ObterUsuarioPorId_RetornaUsuario()
+    {
+        // act 
+        var usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
+
+        // assert
+        Assert.NotNull(usuario);
+    }
+
+    [Fact]
+    public async void ObterUsuarioPorId_RetornaUsuarioComNomeCorreto()
+    {
+        // act 
+        var usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
+
+        // assert
+        Assert.NotNull(usuario);
+        Assert.Equal(_nomeUsuario, usuario.Nome);
+    }
+
+    [Fact]
+    public async void SalvarUsuario_RetornaUsuarioComNomeAlterado()
+    {
+        // act 
+        Usuario? usuario;
+        if (await _usuarioRepositorio.ObterPorIdAsync(_idUsuario) is Usuario u)
+        {
+            u.Atualizar(Usuario.Fabrica.Criar(_outroNome, _emailUsuario));
+            _usuarioRepositorio.Atualizar(u);
+            _contexto.SaveChanges();
+        }
+
+        usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
+
+        // assert
+        Assert.NotNull(usuario);
+        Assert.Equal(_outroNome, usuario.Nome);
+    }
+
+    [Fact]
+    public async void CriarUsuario_RetornaUsuario()
+    {
+        // act 
+        var usuario = Usuario.Fabrica.Criar(_outroNome, _outroEmail);
+
+        await _usuarioRepositorio.CriarAsync(usuario);
+        await _contexto.SaveChangesAsync();
+
+        // assert
+        Assert.NotNull(await _usuarioRepositorio.ObterPorIdAsync(usuario.IdentificadorExterno));
+        Assert.Equal(_outroNome, usuario.Nome);
+    }
+
+    [Fact]
+    public async void ExcluirUsuario_RetornaVerdadeiro()
+    {
+        // act 
+        var usuario = await _usuarioRepositorio.ObterPorIdAsync(_idUsuario);
+        bool registoExcluido = false;
+
+        if(usuario is not null)
+        {
+            _usuarioRepositorio.Excluir(usuario);
+            registoExcluido = await _contexto.SaveChangesAsync() > 0;
+        }
+
+        // assert
+        Assert.Null(await _usuarioRepositorio.ObterPorIdAsync(_idUsuario));
+        Assert.True(registoExcluido);
+    }
+
+    [Fact]
+    public async void ObterTodosUsuario_RetornaListaUsuarios()
+    {
+        // act 
+        var usuarios = await _usuarioRepositorio.ObterTodosAsync();
+
+
+        // assert
+        Assert.NotNull(usuarios);
+        Assert.True(usuarios is not null and IEnumerable<Usuario>);
+    }
+
 }
---
+++ CIFinance/CIFinance.Testes/Servicos/TesteUnitarioServicoToken.cs
@@ -0,0 +1,75 @@
+﻿using CIFinance.Aplicacao;
+using CIFinance.Aplicacao.Abstracoes;
+using CIFinance.Aplicacao.Servicos;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CIFinance.Testes.Servicos;
+
+public class TesteUnitarioServicoToken
+{
+    private readonly IToken _servicoToken;
+    private readonly IConfiguracaoToken _configuracaoToken;
+    private readonly string _userName = "igoreusttaquio@gmail.com";
+    private readonly string _userId = "asty23wk";
+    private readonly IGeradorChave _geradorChves;
+
+
+    private readonly List<Claim> _claims;
+    public TesteUnitarioServicoToken()
+    {
+
+        _geradorChves = new GeradorChaveServico();
+
+        string base64Key = _geradorChves.GerarChaveBase64();
+
+        _claims =
+        [
+            new (JwtRegisteredClaimNames.Sub, _userName),
+            new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new ("userId", _userId)
+        ];
+
+        _servicoToken = new TokenServico();
+        _configuracaoToken = new ConfiguracaoToken()
+        {
+            Audiencia = "testes.token",
+            ChaveSecreta = base64Key,
+            Emissor = "testes.token",
+            Minutos = 2.2
+        };
+    }
+
+    [Fact]
+    public void VerificaGeracaoToken_RetornaVerdadero()
+    {
+        // act
+        var token = _servicoToken.GerarToken(_configuracaoToken, [.. _claims]);
+
+        // asert 
+        Assert.NotNull(token);
+    }
+
+    [Fact]
+    public void ValidacaoTokenGerado_RetornaVerdadeiro()
+    {
+        // act
+        var token = _servicoToken.GerarToken(_configuracaoToken, [.. _claims]);
+
+        // assert
+        var claimsPrincial = _servicoToken.ValidarToken(token, _configuracaoToken);
+        Assert.NotNull(claimsPrincial);
+    }
+
+    [Fact] 
+    public void ValidarUsuarioIdTokenGerado_RetoronaVerdadeiro()
+    {
+        // act
+        var token = _servicoToken.GerarToken(_configuracaoToken, [.. _claims]);
+
+        // assert
+        var claimsPrincial = _servicoToken.ValidarToken(token, _configuracaoToken);
+        Assert.NotNull(claimsPrincial);
+        Assert.Equal(claimsPrincial.Value.Identidades.FindFirst("userId")?.Value, _userId);
+    }
+}
