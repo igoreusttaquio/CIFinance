4738955b9f976bc48bfe3fdbaee8348d36d51a83
Igor E. Silva <igoreusttaquio@gmail.com>
2024-09-16 20:08:36 -0300

    Testes unitarios para o result pattern.

---
+++ CIFinance/CIFinance.Testes/Dominio/TesteUnitarioPadraoResultados.cs
@@ -0,0 +1,22 @@
+﻿using CIFinance.Dominio.Abstracoes;
+
+namespace CIFinance.Testes.Dominio;
+
+public class TesteUnitarioPadraoResultados
+{
+    [Fact]
+    public void TestaPadraoResultadoDominio_RetornaVerdadeiro()
+    {
+        // arrange
+        Resultado<string> resultado;
+
+        // act
+        var umaString = "conversaoImplicita";
+        resultado = umaString;
+
+        // assert
+        Assert.True(resultado.Exitou);
+        Assert.Equal(resultado.Valor, umaString);
+        Assert.NotEqual(resultado.GetType(), umaString.GetType());
+    }
+}
